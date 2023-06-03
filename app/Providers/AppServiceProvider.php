9e7e65913692e44f40eb8c00aa90742af51112bc
odori7353 <fudpen2@gmail.com>
2023-06-03 14:55:02 +0900

    03の修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,15 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+    public function boot(){
+       Paginator::useBootstrap();
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+       // Paginator::useBootstrapFive();
+       //または Paginator::useBootstrapFour();
     }
 }
